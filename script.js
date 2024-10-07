f9febcf948c58029732f13307a0419118ca8e5b9
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-07 23:18:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const generateButton = document.getElementById("generate");
+  const promptInput = document.getElementById("prompt");
+  const heightInput = document.getElementById("hInput");
+  const widthInput = document.getElementById("wInput");
+  const imageContainer = document.getElementById("image-container");
+  const loadingSpinner = document.getElementById("loading-spinner");
+  const genText = document.getElementById("generatedText");
+  const dlButton = document.getElementById("dlButton");
+
+  generateButton.addEventListener("click", function() {
+    var description = promptInput.value ? encodeURIComponent(promptInput.value) : "beautiful%20landscape";
+    const randomSeed = Math.floor(Math.random() * 1000000000);
+    const heightA = heightInput.value ? parseInt(heightInput.value) : 360;
+    const widthA = widthInput.value ? parseInt(widthInput.value) : 480;
+
+    loadingSpinner.style.display = "block";
+    genText.style.display = "none";
+    generateButton.classList.add("hide");
+
+    const imageUrl = `https://image.pollinations.ai/prompt/${description}?nologo=1&seed=${randomSeed}&height=${heightA}&width=${widthA}`;
+    console.log("URL: " + imageUrl);
+    const image = document.createElement("img");
+
+    image.onload = function() {
+      loadingSpinner.style.display = "none";
+      genText.style.display = "block";
+      document.getElementById("dlButton").setAttribute("href", imageUrl);
+      dlButton.style.display = "block";
+      generateButton.classList.remove("hide");
+      imageContainer.innerHTML = "";
+      imageContainer.appendChild(image);
+    };
+
+    image.src = imageUrl;
+  });
+});
